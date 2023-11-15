9a584e71855086ef34562fd4767747a633c62e14
Anfiar <artykishkevich@gmail.com>
2023-11-16 00:56:39 +0300

    Finish VekPage and VekTest class.

--- src/main/java/by/itacademy/kishkevich/VekPage.java
+++ src/main/java/by/itacademy/kishkevich/VekPage.java
@@ -3,7 +3,13 @@ package by.itacademy.kishkevich;
 public class VekPage {
     String enterToAccountFormButtonLocator = "//*[@id=\"header\"]/div/div[3]/div/div[3]/div/div/div/button";
     String enterToLoginFormButtonLocator = "//*[@id=\"userToolsDropDown\"]/div/div[1]/div[2]/button";
-    String loginSubmitButtonLocator = "//*[@id=\"default-:r0:-modal\"]/div/div/div[2]/div/div/form/div/div[3]/button";
+    String loginSubmitButtonLocator = "//*[@id=\"default-:R2mm:-modal\"]/div/div/div[2]/div/div/form/div/div[3]/button";
     String loginFormEmailInputLocator = "//*[@id=\"login-email\"]";
     String loginFormPasswordInputLocator = "//*[@id=\"login-password\"]";
+    String url = "https://www.21vek.by/";
+    String loginEmailErrorMessage = "//*[@id=\"default-:R2mm:-modal\"]/div/div/div[2]/div/div/form/div/div[1]/div[3]/span[2]";
+    String loginPasswordErrorMessage = "//*[@id=\"default-:R2mm:-modal\"]/div/div/div[2]/div/div/form/div/div[2]/div[3]";
+    String declineCookieFormLocator = "//*[@id=\"modal-cookie\"]/div/div[2]/div/button[2]";
+    String declineCookieFormConfirmLocator = "//*[@id=\"modal-cookie\"]/div/div[2]/div/button[2]";
+    String cookieFormLocator = "//*[@id=\"modal-cookie\"]/div";
 }
--- src/test/java/by/itacademy/kishkevich/VekTest.java
+++ src/test/java/by/itacademy/kishkevich/VekTest.java
@@ -1,24 +1,97 @@
 package by.itacademy.kishkevich;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VekTest {
     @Test
-    public void testLoginWithEmptyField() {
+    public void testLoginWithEmptyField() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        VekPage vekPage = new VekPage();
+        driver.get(vekPage.url);
+
+        if (driver.findElement(By.xpath(vekPage.cookieFormLocator)) != null) {
+            driver.findElement(By.xpath(vekPage.declineCookieFormLocator)).click();
+            driver.findElement(By.xpath(vekPage.declineCookieFormConfirmLocator)).click();
+        }
+
+        driver.findElement(By.xpath(vekPage.enterToAccountFormButtonLocator)).click();
+        driver.findElement(By.xpath(vekPage.enterToLoginFormButtonLocator)).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(vekPage.loginSubmitButtonLocator)).click();
+        assertEquals("Электронная почта не указана", driver.findElement(By.xpath(vekPage.loginEmailErrorMessage)).getText());
+        assertEquals("Пароль не указан", driver.findElement(By.xpath(vekPage.loginPasswordErrorMessage)).getText());
+        driver.quit();
     }
 
     @Test
-    public void testLoginWithoutEmail(){
+    public void testLoginWithoutEmail() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        VekPage vekPage = new VekPage();
+        driver.get(vekPage.url);
 
+        if (driver.findElement(By.xpath(vekPage.cookieFormLocator)) != null) {
+            driver.findElement(By.xpath(vekPage.declineCookieFormLocator)).click();
+            driver.findElement(By.xpath(vekPage.declineCookieFormConfirmLocator)).click();
+        }
+
+        driver.findElement(By.xpath(vekPage.enterToAccountFormButtonLocator)).click();
+        driver.findElement(By.xpath(vekPage.enterToLoginFormButtonLocator)).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(vekPage.loginFormPasswordInputLocator)).sendKeys("123456789");
+        driver.findElement(By.xpath(vekPage.loginSubmitButtonLocator)).click();
+        assertEquals("Электронная почта не указана", driver.findElement(By.xpath(vekPage.loginEmailErrorMessage)).getText());
+        driver.quit();
     }
 
     @Test
-    public void testLoginWithoutPassword(){
+    public void testLoginWithoutPassword() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        VekPage vekPage = new VekPage();
+        driver.get(vekPage.url);
 
+        if (driver.findElement(By.xpath(vekPage.cookieFormLocator)) != null) {
+            driver.findElement(By.xpath(vekPage.declineCookieFormLocator)).click();
+            driver.findElement(By.xpath(vekPage.declineCookieFormConfirmLocator)).click();
+        }
+
+        driver.findElement(By.xpath(vekPage.enterToAccountFormButtonLocator)).click();
+        driver.findElement(By.xpath(vekPage.enterToLoginFormButtonLocator)).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(vekPage.loginFormEmailInputLocator)).sendKeys("test889@gmail.com");
+        driver.findElement(By.xpath(vekPage.loginSubmitButtonLocator)).click();
+        assertEquals("Пароль не указан", driver.findElement(By.xpath(vekPage.loginPasswordErrorMessage)).getText());
+        driver.quit();
     }
 
     @Test
-    public void testLoginByNotRegisteredUser() {
+    public void testLoginByNotRegisteredUser() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        VekPage vekPage = new VekPage();
+        driver.get(vekPage.url);
 
+        if (driver.findElement(By.xpath(vekPage.cookieFormLocator)) != null) {
+            driver.findElement(By.xpath(vekPage.declineCookieFormLocator)).click();
+            driver.findElement(By.xpath(vekPage.declineCookieFormConfirmLocator)).click();
+        }
+
+        driver.findElement(By.xpath(vekPage.enterToAccountFormButtonLocator)).click();
+        driver.findElement(By.xpath(vekPage.enterToLoginFormButtonLocator)).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(vekPage.loginFormEmailInputLocator)).sendKeys("test889@gmail.com");
+        driver.findElement(By.xpath(vekPage.loginFormPasswordInputLocator)).sendKeys("123456789");
+        driver.findElement(By.xpath(vekPage.loginSubmitButtonLocator)).click();
+        Thread.sleep(1000);
+        assertEquals("Нет такого аккаунта. \n" +
+                "Зарегистрироваться?", driver.findElement(By.xpath(vekPage.loginEmailErrorMessage)).getText());
+        driver.quit();
     }
 }

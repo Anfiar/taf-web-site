239ec8dad1f2d741ca1314e6f7184ce6137d0fde
Anfiar <artykishkevich@gmail.com>
2023-11-16 00:06:36 +0300

    Finish OzPage and OzTest class.

--- src/main/java/by/itacademy/kishkevich/OzPage.java
+++ src/main/java/by/itacademy/kishkevich/OzPage.java
@@ -6,4 +6,7 @@ public class OzPage {
     String loginFormEmailInputLocator = "//*[@id=\"loginForm\"]/div[2]/div[1]/div[1]/input";
     String loginFormPasswordInputLocator = "//*[@id=\"loginForm\"]/div[2]/div[1]/div[2]/input";
     String loginFormSubmitButtonLocator = "//*[@id=\"loginForm\"]/button";
+    String url = "https://oz.by/";
+    String loginErrorMessage = "/html/body/div[1]/div/div/div[1]/div/div/div[1]/div[3]/div/div[1]/form/div[2]/div[2]/div/div/div";
+    String loginPopupIntroLocator = "//*[@id=\"loginPopupIntro\"]";
 }
--- src/test/java/by/itacademy/kishkevich/OzTest.java
+++ src/test/java/by/itacademy/kishkevich/OzTest.java
@@ -1,15 +1,38 @@
 package by.itacademy.kishkevich;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OzTest {
     @Test
-    public void testLoginByNotRegisteredUser() {
-
+    public void testLoginByNotRegisteredUser() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        OzPage ozPage = new OzPage();
+        driver.get(ozPage.url);
+        driver.findElement(By.xpath(ozPage.enterToLoginFormButtonLocator)).click();
+        driver.findElement(By.xpath(ozPage.loginFormByEmailLocator)).click();
+        driver.findElement(By.xpath(ozPage.loginFormEmailInputLocator)).sendKeys("test99@gmail.com");
+        driver.findElement(By.xpath(ozPage.loginFormPasswordInputLocator)).sendKeys("12345");
+        driver.findElement(By.xpath(ozPage.loginFormSubmitButtonLocator)).click();
+        Thread.sleep(1000);
+        assertEquals("Адрес электронной почты не зарегистрирован. Зарегистрироваться", driver.findElement(By.xpath(ozPage.loginErrorMessage)).getText());
+        driver.quit();
     }
 
     @Test
-    public void testEnterButton() {
-
+    public void testEnterButton() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        OzPage ozPage = new OzPage();
+        driver.get(ozPage.url);
+        driver.findElement(By.xpath(ozPage.enterToLoginFormButtonLocator)).click();
+        Thread.sleep(1000);
+        assertEquals("Вход", driver.findElement(By.xpath(ozPage.loginPopupIntroLocator)).getText());
+        driver.quit();
     }
 }

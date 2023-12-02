e2b6a79329a4d86ef826364cc2577c072b53cf83
Anfiar <artykishkevich@gmail.com>
2023-12-02 11:51:51 +0300

    Add new test for PizzaTempoTest.

--- src/main/java/by/itacademy/kishkevich/PizzaTempoPage.java
+++ src/main/java/by/itacademy/kishkevich/PizzaTempoPage.java
@@ -5,12 +5,15 @@ import org.openqa.selenium.WebDriver;
 
 public class PizzaTempoPage {
     private String url = "https://www.pizzatempo.by/";
-    private String pizzaButton = "//img[@alt=\"Пицца\"]";
-    private String margaritaOrderButton = "//*[@id=\"1\"]//div[@class=\"order-btn\"]";
-    private String margaritaSizeButton = "//*[@id=\"1\"]//div[@class=\"pizza-size choiser\"]//div[3]";
-    private String addToBasketButton = "//div[@id=\"order_pizza_popup\"]//div[@class=\"order-btn\"]";
-    private String basketButton = "//div[@class=\"basket-top\"]/div[@class=\"bs-price-col\"]";
-    private String itemInBasket = "//div[@id=\"basket_expand\"]//div[@class=\"cart-product-name title\"]";
+    private String pizzaButton = "//img[@alt='Пицца']";
+    private String margaritaOrderButton = "//*[@id='1']//div[@class='order-btn']";
+    private String margaritaSizeButton = "//*[@id='1']//div[@class='pizza-size choiser']//div[3]";
+    private String addToBasketButton = "//div[@id='order_pizza_popup']//div[@class='order-btn']";
+    private String basketButton = "//div[@class='basket-top']/div[@class='bs-price-col']";
+    private String pizzaInBasket = "//div[@id='basket_expand']//tr[@class='item cart-item  ']//div[@class='cart-product-name title']";
+    private String drinkButton = "//div[3]//li[6]";
+    private String bonaquaOrderButton = "//button[@rel='6012-1']";
+    private String drinkInBasket = "//div[@id='basket_expand']//tr[@class='item cart-item   chet']//div[@class='cart-product-name title']";
     private WebDriver driver;
 
     public PizzaTempoPage(WebDriver driver) {
@@ -41,7 +44,20 @@ public class PizzaTempoPage {
         driver.findElement(By.xpath(basketButton)).click();
     }
 
-    public String getTextItemInBasket() {
-        return driver.findElement(By.xpath(itemInBasket)).getText();
+    public String getTextPizzaInBasket() {
+        return driver.findElement(By.xpath(pizzaInBasket)).getText();
     }
+
+    public void clickDrinkButton() {
+        driver.findElement(By.xpath(drinkButton)).click();
+    }
+
+    public String getTextDrinkInBasket() {
+        return driver.findElement(By.xpath(drinkInBasket)).getText();
+    }
+
+    public void clickBonuaquaOrderButton() {
+        driver.findElement(By.xpath(bonaquaOrderButton)).click();
+    }
+
 }
--- src/test/java/by/itacademy/kishkevich/PizzaTempoTest.java
+++ src/test/java/by/itacademy/kishkevich/PizzaTempoTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PizzaTempoTest extends BaseTest {
-    private String itemInBusketExpectedResult = "Пицца \"Маргарита\" (31см)";
+    private String pizzaInBusketExpectedResult = "Пицца \"Маргарита\" (31см)";
+    private String drinkInBusketExpectedResult = "Вода мин. Бонаква среднегазированая 1л";
 
     @Test
     public void testOrderMargarita() throws InterruptedException {
@@ -17,6 +18,24 @@ public class PizzaTempoTest extends BaseTest {
         pizzaTempoPage.clickAddToBasketButton();
         Thread.sleep(1000);
         pizzaTempoPage.clickBasketButton();
-        Assertions.assertEquals(itemInBusketExpectedResult, pizzaTempoPage.getTextItemInBasket());
+        Assertions.assertEquals(pizzaInBusketExpectedResult, pizzaTempoPage.getTextPizzaInBasket());
+    }
+
+    @Test
+    public void testOrderMargaritaAndBonaqua() throws InterruptedException {
+        PizzaTempoPage pizzaTempoPage = new PizzaTempoPage(driver);
+        pizzaTempoPage.getPage();
+        pizzaTempoPage.clickPizzaButton();
+        pizzaTempoPage.clickMargaritaSizeButton();
+        pizzaTempoPage.clickMargaritaOrderButton();
+        Thread.sleep(1000);
+        pizzaTempoPage.clickAddToBasketButton();
+        Thread.sleep(1000);
+        pizzaTempoPage.clickDrinkButton();
+        pizzaTempoPage.clickBonuaquaOrderButton();
+        Thread.sleep(1000);
+        pizzaTempoPage.clickBasketButton();
+        Assertions.assertEquals(pizzaInBusketExpectedResult, pizzaTempoPage.getTextPizzaInBasket());
+        Assertions.assertEquals(drinkInBusketExpectedResult, pizzaTempoPage.getTextDrinkInBasket());
     }
 }
